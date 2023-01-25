e710ce5141a1b9c14f35e5ffdf545b493f060578
Gonzalo Plaza <me@gonzaloplaza.com>
2023-01-25 18:11:07 +0100

    chore: update dependencies, fix unit tests, fix config (#280)
    Co-authored-by: Gonzalo Plaza <gonzalo.plaza@aircall.io>

--- jest.config.ts
+++ jest.config.ts
@@ -60,11 +60,7 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    'ts-jest': {
-      compiler: 'ttypescript'
-    }
-  },
+  // globals: {},
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -174,7 +170,7 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: { '^.+\\.ts?$': 'ts-jest' }
+  transform: { '^.+\\.ts?$': ['ts-jest', { compiler: 'ttypescript' }] }
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
